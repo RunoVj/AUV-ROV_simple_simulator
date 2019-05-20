ebad622a40eafddd0285f9824171fdd0ccb0d81b
RunoVj <runodj@gmail.com>
2019-05-21 01:28:03 +0300

    Added all ROV twists

--- src/rov_twist/scripts/move.py
+++ src/rov_twist/scripts/move.py
@@ -1,89 +1,111 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
-
 from geometry_msgs.msg import Twist
+import asyncio
+
+host = "127.0.0.1"
+port = 9090
 
 PI = 3.1415926535897
 
 
-class movement :
+class Movement:
 
     def __init__(self):
         rospy.init_node('move_robot_node', anonymous=False)
-        self.pub_move = rospy.Publisher("/cmd_vel",Twist, queue_size=1)
-        self.rate = rospy.Rate(1)
+        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+        self.rate = rospy.Rate(25)
+        self.move = Twist()
 
-
-    def move_forward(self):
-        print "move forward"
-        move = Twist()
-        move.linear.x = 1.0 
-        move.angular.z = 0.0
-        for i in range(10):
-            move.linear.x += 0.1
-            self.pub_move.publish(move)
-            self.rate.sleep()
-            print move
-        print "stop"
-        move.linear.x = 0
+    def publish(self):
+        # publish only once
         while not rospy.is_shutdown():
-            self.pub_move.publish(move)
-            self.rate.sleep()
-            print move
+            connections = move.pub.get_num_connections()
+            if connections > 0:
+                self.pub.publish(self.move)
+                print(f"Publish twist: {self.move}")
+                break
+            move.rate.sleep()
 
+    def reset(self):
+        self.move.linear.x = 0.0
+        self.move.linear.y = 0.0
+        self.move.linear.z = 0.0
+        self.move.angular.x = 0.0
+        self.move.angular.y = 0.0
+        self.move.angular.z = 0.0
 
-    def move_backward(self):
-        move =Twist()
-        move.linear.x=-1
-        move.angular.z=0.0
-        self.pub_move.publish(move)
+    def forward(self, speed):
+        print("move forward")
+        self.reset()
+        self.move.linear.x = speed
+        self.publish()
+
+    def backward(self, speed):
+        print("move forward")
+        self.reset()
+        self.move.linear.x -= speed;
+        self.publish()
+
+    def left(self, speed):
+        print("move left")
+        self.reset()
+        self.move.linear.y = speed
+        self.publish()
+
+    def right(self, speed):
+        print("move right")
+        self.reset()
+        self.move.linear.y -= speed;
+        self.publish()
+
     def stop(self):
-        move =Twist()
-        move.linear.x=0
-        move.angular.z=0.0
-        self.pub_move.publish(move)
+        self.reset()
+        self.publish()
 
-    def rotate_right(self):
-        move =Twist()
-        # Receiveing the user's input
-        print("Let's rotate your robot")
-        speed = 100
-        angle = 90
-        clockwise = True #True or false
+    def rotate_left(self, speed):
+        print("move left")
+        self.reset()
+        self.move.angular.z = speed
+        self.publish()
 
-        #Converting from angles to radians
-        angular_speed = speed*2*PI/360
-        relative_angle = angle*2*PI/360
+    def rotate_right(self, speed):
+        print("move right")
+        self.reset()
+        self.move.linear.z -= speed;
+        self.publish()
 
-            #We wont use linear components
-        move.linear.x=0
-        move.linear.y=0
-        move.linear.z=0
-        move.angular.x = 0
-        move.angular.y = 0
 
-            # Checking if our movement is CW or CCW
-        if clockwise:
-            move.angular.z = -abs(angular_speed)
-            print move.angular.z
-        else:
-            move.angular.z = abs(angular_speed)
-        # Setting the current time for distance calculus
-        t0 = rospy.Time.now().to_sec()
-        current_angle = 0
+move = Movement()
 
-        while(current_angle < relative_angle):
-            self.pub_move.publish(move)
-            t1 = rospy.Time.now().to_sec()
-            current_angle = angular_speed*(t1-t0)
-        #Forcing our robot to stop
-        move.angular.z = 0
-        self.pub_move.publish(move)
-        rospy.spin()
 
-while not rospy.is_shutdown() :
+class UDPServer:
+    def connection_made(self, transport):
+        self.transport = transport
 
-    mov = movement()
-    mov.move_forward()
-    rospy.spin()
+    def datagram_received(self, data, addr):
+        message = data
+        print('Received %r from %s' % (int(message[-1]), addr))
+
+        move.rotate_left(int(message[-1])/255)
+
+
+async def main():
+    print("Starting UDP server")
+    loop = asyncio.get_running_loop()
+
+    # One protocol instance will be created to serve all
+    # client requests.
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: UDPServer(), local_addr=(host, port))
+
+    print(f"Created server: \nhost - {host}, port - {port}")
+    try:
+        await asyncio.sleep(60)  # Serve for 1 minute.
+    finally:
+        move.stop()
+        transport.close()
+
+
+asyncio.run(main())
